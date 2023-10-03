e2ef1144d5fb67c4f0ecb4ddbdaa3ce96cfa49bf
Arpan Swain <kankalase07@gmail.com>
2023-10-03 12:56:25 +0530

    Added send email feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail,Message
 
 app = Flask(__name__)
 
@@ -8,6 +9,16 @@ app.config["SECRET_KEY"] = "myapp123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 #db is an instance of SQLAlchemy class
 db = SQLAlchemy(app)
+
+#FOR SENDING MAIL
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] =True
+app.config["MAIL_USERNAME"] = "arpanswain754@gmail.com"
+app.config["MAIL_PASSWORD"] = "qvvf acqz vaxb boet"
+
+mail = Mail(app)
+
 
 #create a datbase model...db.model is also a class
 class Form(db.Model):
@@ -32,6 +43,17 @@ def index():
 
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank You for Submission, {first_name}. "\
+        f"Here are your details :\n {first_name}\n{last_name}\n{date}\n"\
+        f" Thank You !"
+
+        message = Message(subject="New Form Submission",
+                          sender = app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+
+        mail.send(message)
 
         flash(f"{first_name}, Your form was submitted successfully !", "success")
 
